a041b25f18ba50517bf24d70bf8c69dd6322fb92
jamesyu282243 <150282927+jamesyu282243@users.noreply.github.com>
2023-12-04 13:03:22 -0500

    mirror elephant movements

--- Elephant.java
+++ Elephant.java
@@ -9,18 +9,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage[] idle = new GreenfootImage[8];;
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+
+    
+    // direction the elephant is facing
+    String facing = "right";
     /**
      * Constructor - the code that gets run one time when object is created
      */
     public Elephant()
     {
-        for(int i = 0; i < idle.length; i++)
+        for(int i = 0; i < idleRight.length; i++)
         {
-            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
-            idle[i].scale(100, 100);
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
         }
-        setImage(idle[0]);
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        //initial elephant image
+        setImage(idleRight[0]);
         
     }
     /**
@@ -30,26 +42,40 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
-        setImage(idle[imageIndex]);
-        imageIndex = (imageIndex + 1) % idle.length;
-    }
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+    
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+
+        }
+    }    
     public void act()
     {
         if(Greenfoot.isKeyDown("a"))
         {
             move(-2);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("d"))
         {
             move(2);
+            facing = "right";
         }
         if(Greenfoot.isKeyDown("left"))
         {
             move(-2);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
             move(2);
+            facing = "right";
         }
         
         
